8bb709e241410e187637c1e70fdfd5b2cd96207c
duarte <duartebrrsmorais@gmail.com>
2014-12-04 18:10:16 +0000

    atualizacao da base dados

--- database/database.sql
+++ database/database.sql
@@ -53,26 +53,75 @@ create table UsersPoll(
 	poll integer REFERENCES Poll(id) ON DELETE CASCADE
 );
 
+--------------------------------------------- user ---------------------------------------------------
+insert into User values('teste', '5-05-1994', 'Susana', 'Ventura', 'teste@mail.pt', '000');
+insert into User values('CASW134', '12-02-1990', 'Carlos', 'Almeida', 'CASW134@mail.pt', '111');
+insert into User values('AS32_Sil', '25-02-1960', 'Alberto', 'Silva', 'AS32_Sil@mail.pt', '222');
+insert into User values('NL3236_lar', '4-12-1994', 'Nuno', 'Laranja', 'NL3236_lar@mail.pt', '333');
+insert into User values('X_T2313', '5-05-1994', 'Xavier', 'Torrié', 'X_T2313@mail.com.br', '444');
+insert into User values('And_stro_1213', '5-05-1994', 'Andresson', 'Strong', 'And_stro_1213@mail.com', '555');
 
-insert into User values('teste', '5-05-1994', 'susana', 'ventura', 'teste@mail.pt', '123');
-
+--------------------------------------------- poll------------------------------------------------------------
 insert into Poll values(NULL, 'Loret', 'http://www.jornalglobal.com/wp-content/uploads/2014/10/chocolate.jpg', 0, 'teste', 'Coiso', 'See Results');
-insert into Poll values(NULL, 'Ipsum', 'http://placehold.it/200&text=Many%20Polls', 0, 'teste', 'Vote', 'See Results');
+insert into Poll values(NULL, 'Coca-cola', 'http://placehold.it/200&text=Many%20Polls', 0, 'teste', 'Vote', 'See Results');
+insert into Poll values(NULL, 'wine', 'http://placehold.it/200&text=Many%20Polls', 1, 'CASW134', 'Vote', 'See Results');
+insert into Poll values(NULL, 'Fanta', 'http://placehold.it/200&text=Many%20Polls', 2, 'CASW134', 'Vote', 'See Results');
+insert into Poll values(NULL, 'security', 'http://placehold.it/200&text=Many%20Polls', 0, 'AS32_Sil', 'Vote', 'See Results');
+insert into Poll values(NULL, 'cooking', 'http://placehold.it/200&text=Many%20Polls', 1, 'AS32_Sil', 'Vote', 'See Results');
+insert into Poll values(NULL, 'Coffee', 'http://placehold.it/200&text=Many%20Polls', 2, 'NL3236_lar', 'Vote', 'See Results');
 
+-------------------------------------------Question--------------------------------------------------
 insert into Question values(NULL, 'Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae?', 1, 0);
-insert into Question values(NULL, 'ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae?', 2, 1);
-
+insert into Question values(NULL, 'Is coca-cola bad to the human body?', 2, 1);
+insert into Question values(NULL, 'Is Porto wine the best wine in the world?', 3, 0);
+insert into Question values(NULL, 'Witch fanta is the best fanta?', 4, 1);
+insert into Question values(NULL, 'What is the level of security you feel in Portutal? 1 = bad and 5 = very good', 5, 0);
+insert into Question values(NULL, 'Do you like cooking?', 6, 1);
+insert into Question values(NULL, 'Do you thing about coffee?', 7, 1);
+------------------------------------------PossibleAnswer--------------------------------------------------------
 insert into PossibleAnswer values(NULL, 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.', 1);
 insert into PossibleAnswer values(NULL, 'Integer a lorem ac ex tristique consectetur eget at metus.', 1);
-insert into PossibleAnswer values(NULL, 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.', 2);
-insert into PossibleAnswer values(NULL, 'Integer a lorem ac ex tristique consectetur eget at metus.', 2);
+insert into PossibleAnswer values(NULL, 'Yes.', 2);
+insert into PossibleAnswer values(NULL, 'No.', 2);
+insert into PossibleAnswer values(NULL, 'Yes.', 3);
+insert into PossibleAnswer values(NULL, 'No.', 3);
+insert into PossibleAnswer values(NULL, 'Orange.', 4);
+insert into PossibleAnswer values(NULL, 'Lemon.', 4);
+insert into PossibleAnswer values(NULL, 'Strawberry.', 4);
+insert into PossibleAnswer values(NULL, 'Pineapple.', 4);
+insert into PossibleAnswer values(NULL, '1.', 5);
+insert into PossibleAnswer values(NULL, '2.', 5);
+insert into PossibleAnswer values(NULL, '3.', 5);
+insert into PossibleAnswer values(NULL, '4.', 5);
+insert into PossibleAnswer values(NULL, '5.', 5);
+insert into PossibleAnswer values(NULL, 'Yes.', 6);
+insert into PossibleAnswer values(NULL, 'No.', 6);
+insert into PossibleAnswer values(NULL, 'Yes.', 7);
+insert into PossibleAnswer values(NULL, 'No.', 7);
 
+---------------------------------------UserAnswerPoll-------------------------------------------------------------
+insert into UserAnswerPoll values('teste', 1);
+insert into UserAnswerPoll values('CASW134', 1);
+insert into UserAnswerPoll values('AS32_Sil', 1);
+insert into UserAnswerPoll values('AS32_Sil', 1);
+insert into UserAnswerPoll values('NL3236_lar', 1);
+insert into UserAnswerPoll values('NL3236_lar', 1);
+insert into UserAnswerPoll values('NL3236_lar', 1);
+insert into UserAnswerPoll values('X_T2313', 1);
+insert into UserAnswerPoll values('X_T2313', 1);
+insert into UserAnswerPoll values('X_T2313', 1);
+insert into UserAnswerPoll values('X_T2313', 1);
 insert into UserAnswerPoll values('teste', 1);
 
+
+-------------------------------------------UsersPoll-------------------------------------------------------------
 insert into UsersPoll values('teste', 1);
 insert into UsersPoll values('teste', 2);
-
-
+insert into UsersPoll values('CASW134', 3);
+insert into UsersPoll values('teste', 4);
+insert into UsersPoll values('teste', 5);
+insert into UsersPoll values('AS32_Sil', 6);
+insert into UsersPoll values('AS32_Sil', 7);
 
 
 
